b71520a64d6119f74d312b04fcab5411f7d9624e
khush045six <khushboorastogi2012@gmail.com>
2025-01-13 21:12:38 +0530

    Update BasicsOfTestng.java

--- src/test/java/testNGclasses/BasicsOfTestng.java
+++ src/test/java/testNGclasses/BasicsOfTestng.java
@@ -20,6 +20,7 @@ public class BasicsOfTestng {
     public void testCase2(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://www.amazon.in/");
+      driver.manage().window().maximize();
         driver.close();
     }
 
