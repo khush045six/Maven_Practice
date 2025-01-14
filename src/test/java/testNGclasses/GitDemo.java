b2a692ef41049db1c95299429f9b88ac83764944
khush045six <khushboorastogi2012@gmail.com>
2025-01-14 12:06:55 +0530

    Update GitDemo.java

--- src/test/java/testNGclasses/GitDemo.java
+++ src/test/java/testNGclasses/GitDemo.java
@@ -3,6 +3,9 @@ package testNGclasses;
 public class GitDemo {
     public static void main(String[] args) {
         System.out.println("Hello Friends");
+        System.out.println("My Name Is Khushboo Rastogi");
+        
+            
 
     }
 }
